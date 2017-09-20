0d9060981ce4798687d1c027621e0820050777c4
Tanisha <tanisha_498@hotmail.com>
2017-09-20 17:13:09 -0600

    inicializacion de proyecto

---
+++ src/clientes/Clientes.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientes;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tanisha
+ */
+public class Clientes {
+    String nit, telefono;
+    String nombre,direccion, email;
+    int saldo=0;
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        String nombre = JOptionPane.showInputDialog("Ingrese nombre:");
+        String nit = JOptionPane.showInputDialog("Ingrese Nit: ");
+        String Direccion = JOptionPane.showInputDialog("Ingrese direccion:");
+        String telefono = JOptionPane.showInputDialog("Ingrese telefono:");
+        String email = JOptionPane.showInputDialog("Ingrese email:");
+        JOptionPane.showMessageDialog(null, "Su saldo inicial es de Q.0.00");
+    }
+ 
+}
+    
+
+
